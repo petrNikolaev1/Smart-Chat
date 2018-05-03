6fa4676a390910219a13932daaccdbc9b58df514
petrNikolaev1 <petru4o144@gmail.com>
2018-05-03 04:51:15 +0300

    Update privateTyping.js

--- client/src/reducers/privateTyping.js
+++ client/src/reducers/privateTyping.js
@@ -3,12 +3,10 @@ import constants from 'constants'
 function updateState(state, from, flag) {
 	for (var i = 0; i < state.length; i++) {
 		if (state[i].from === from) {
-			console.log('index found', i)
 			state[i].flag = flag
 			return Array.from(state)
 		}
 	}
-	console.log('index not found')
 	return state.concat({from, flag});
 }
 
